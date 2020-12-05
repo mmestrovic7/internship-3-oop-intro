777c5265c5951ebabc2e269d0cd1407e17022bf1
[mmestrovic7] <[marijamestrovic47@gmail.com]>
2020-12-05 19:13:21 +0100

    Added default menu

--- oop3/oop3/Program.cs
+++ oop3/oop3/Program.cs
@@ -32,8 +32,57 @@ namespace oop3
                 {exampleEvent2,exampleGuestList2 },
                 {exampleEvent3,exampleGuestList3 },
 
+            };
+            
+            Console.WriteLine("1.Dodavanje eventa");
+            Console.WriteLine("2.Brisanje eventa");
+            Console.WriteLine("3.Edit eventa");
+            Console.WriteLine("4.Dodavanje osobe na event");
+            Console.WriteLine("5.Uklanjanje osobe sa eventa");
+            Console.WriteLine("6.Ispis detalja eventa.");
+            Console.WriteLine("Izaberite što želite:");
+            
+            bool rightInput = false;
+            while (!rightInput)
+            {
+                bool isItANumber = false;
+                int parsedOption = 0;
+                while (!isItANumber)
+                {
+                    var option = Console.ReadLine();
+                    isItANumber = int.TryParse(option, out parsedOption);
+                    if (!isItANumber)
+                        Console.WriteLine("Morate unijeti broj");
+
+                }
+                switch (parsedOption)
+                {
+                    case 1:
+                        rightInput = true;
+                        break;
+                    case 2:
+                        rightInput = true;
+                        break;
+                    case 3:
+                        rightInput = true;
+                        break;
+                    case 4:
+                        rightInput = true;
+                        break;
+                    case 5:
+                        rightInput = true;
+                        break;
+                    case 6:
+                        rightInput = true;
+                        break;
+                    default:
+                        Console.WriteLine("Morate unijeti broj od 1 do 6");
+                        break;
+                }
             }
-            ;
+
+
+            
 
 
 
