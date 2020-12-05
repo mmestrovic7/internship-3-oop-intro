8af06cfdef1b40a0344d2ae9a507efa49842a883
[mmestrovic7] <[marijamestrovic47@gmail.com]>
2020-12-05 17:58:19 +0100

    Set up class Event

--- oop3/oop3/Event.cs
+++ oop3/oop3/Event.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Text;
 
 namespace oop3
 {
+    public enum EventType{Coffee, Lecture, Concert, StudySession}
     public class Event
     {
+        public Event(string name, EventType eventType, int startTime, int endTime)
+        {
+            Name= name;
+            EventType = eventType;
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+    public string Name { get; set; }
+    public EventType EventType { get; set; }
+    public int StartTime { get; set; }
+    public int EndTime { get; set; }
     }
 }
+
